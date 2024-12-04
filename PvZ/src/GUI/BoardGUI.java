f1288fb1c890a1c050a5cf41edd8ee44ba87c15b
Andr3xC <achavarropp@gmail.com>
2024-12-03 20:35:14 -0500

    Add board grid

--- PvZ/src/GUI/BoardGUI.java
+++ PvZ/src/GUI/BoardGUI.java
@@ -6,6 +6,7 @@ import GUI.extras.BoardBox;
 import GUI.extras.BoardConf;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 
@@ -22,19 +23,13 @@ public class BoardGUI extends JFrame implements Runnable {
     // Dimensions
     private final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
     private static final int ROWS = 5;
-    private static final int COLS = 10;
+    private static final int COLS = 11;
 
     // Panels
     JPanel plantsPanel = new JPanel();
     JPanel zombiesPanel = new JPanel();
     JPanel boardPanel = new BoardConf("PvZ/assets/background/board.jpg");
-
     JPanel infoPanel = new JPanel();
-
-    // Background elements
-    private BackgroundImage backgroundImage;
-    private BackgroundImage backgroundBoard;
-    private BackgroundSound backgroundSound;
 
     // Game elements
     private GameAPP app;
@@ -79,19 +74,13 @@ public class BoardGUI extends JFrame implements Runnable {
         setLocationRelativeTo(null);
         setLayout(new BorderLayout());
 
-        // Background Elements
-        //! Menu sound doesn't stop and the audio duplicates
-        //backgroundSound = new BackgroundSound("PvZ/assets/sound/LoonBoon.wav");
-
-        // Panels organization
-
-
         // Prepare all Elements
         prepareElementsBoard();
         prepareElementsPlayers();
+        prepareElementsInfo();
+        prepareElementsOthers();
 
         // Add Panels
-        add(boardPanel, BorderLayout.CENTER);
         add(plantsPanel, BorderLayout.WEST);
         add(zombiesPanel, BorderLayout.EAST);
         add(infoPanel, BorderLayout.NORTH);
@@ -106,17 +95,25 @@ public class BoardGUI extends JFrame implements Runnable {
                 boardPanel.add(boxes[i][j]);
             }
         }
+
+        boardPanel.setBorder(new EmptyBorder(
+                screenSize.height/9,
+                screenSize.width/8 + 5,
+                screenSize.height/19,
+                screenSize.width/8
+        ));
+        add(boardPanel, BorderLayout.CENTER);
     }
 
     private void prepareElementsPlayers() {
 
-        plantsPanel.setBackground(Color.GREEN);
-        zombiesPanel.setBackground(Color.RED);
-        infoPanel.setBackground(Color.BLUE);
-
-
-        //Plants Panel
+        //* Plants Panel
         plantsPanel.setLayout(new GridLayout(7, 1));
+        plantsPanel.setPreferredSize(new Dimension(screenSize.width/9, screenSize.height/2));
+        plantsPanel.setBackground(new Color(2, 0, 51, 200));
+        plantsPanel.setBorder(BorderFactory.createLineBorder(new Color(2, 0, 51), 8));
+
+        // Elements
         plantsPanel.add(new JLabel("Plants"));
         plantsPanel.add(new JLabel("Suns"));
         plantsPanel.add(new JButton("Sunflower"));
@@ -124,9 +121,16 @@ public class BoardGUI extends JFrame implements Runnable {
         plantsPanel.add(new JButton("Wallnut"));
         plantsPanel.add(new JButton("Cherrybomb"));
         plantsPanel.add(new JButton("Snowpea"));
+        add(plantsPanel, BorderLayout.WEST);
 
-        //Zombies Panel
+
+        //* Zombies Panel
         zombiesPanel.setLayout(new GridLayout(7, 1));
+        zombiesPanel.setPreferredSize(new Dimension(screenSize.width/9, screenSize.height/2));
+        zombiesPanel.setBackground(new Color(2, 0, 51, 200));
+        zombiesPanel.setBorder(BorderFactory.createLineBorder(new Color(2, 0, 51), 8));
+
+        // Elements
         zombiesPanel.add(new JLabel("Zombies"));
         zombiesPanel.add(new JLabel("Brains"));
         zombiesPanel.add(new JButton("Zombie"));
@@ -134,11 +138,24 @@ public class BoardGUI extends JFrame implements Runnable {
         zombiesPanel.add(new JButton("Buckethead"));
         zombiesPanel.add(new JButton("Brainstain"));
         zombiesPanel.add(new JButton("Football"));
+        add(zombiesPanel, BorderLayout.EAST);
+    }
 
+    private void prepareElementsOthers() {
+        JPanel refillPanel = new JPanel();
+        refillPanel.setPreferredSize(new Dimension(screenSize.width, screenSize.height/7));
+        refillPanel.setBackground(new Color(2, 0, 51, 200));
+        refillPanel.setBorder(BorderFactory.createLineBorder(new Color(2, 0, 51), 8));
+        add(refillPanel, BorderLayout.SOUTH);
     }
 
     private void prepareElementsInfo() {
         infoPanel.setLayout(new GridLayout(1, 3));
+        infoPanel.setPreferredSize(new Dimension(screenSize.width, screenSize.height/7));
+        infoPanel.setBackground(new Color(2, 0, 51, 200));
+        infoPanel.setBorder(BorderFactory.createLineBorder(new Color(2, 0, 51), 8));
+
+        // Elements
         timerLabel = new JLabel("Time: 0");
         sunsLabel = new JLabel("Points: 0");
         brainLabel = new JLabel("points: 0");
