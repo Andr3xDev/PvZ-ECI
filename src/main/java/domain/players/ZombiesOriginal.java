534adfbe7b26f3f59ef13cd1f5786e7f8d988890
Andr3xC <achavarropp@gmail.com>
2024-12-12 20:36:42 -0500

    Update docs

--- src/main/java/domain/players/ZombiesOriginal.java
+++ src/main/java/domain/players/ZombiesOriginal.java
@@ -5,16 +5,33 @@ import domain.PvZExceptions;
 
 import java.util.Random;
 
+/**
+ * Class that represents the original zombies player machine, it only drops zombies randomly in the game
+ */
 public class ZombiesOriginal extends Machine {
+
+    //** Attributes **//
 
     private final String[] zombieTypes = {"basic", "conehead", "buckethead", "brainstein", "ecizombie"};
     private int cantZombies;
 
+
+    //** Constructor **//
+
+    /**
+     * Constructor of the original zombies player machine
+     * @param game Game where the player is going to play
+     */
     public ZombiesOriginal(Game game) {
         super(game);
         cantZombies = 0;
     }
 
+
+    /**
+     * Method that makes a move in the game, it drops zombies randomly in the game
+     * @param game Game where the player is going to play
+     */
     @Override
     public void makeMove(Game game) {
         if (cantZombies == 0 || cantZombies % 5 == 0) {
